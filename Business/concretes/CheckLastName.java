efad257d83d5dbedb255d0a00d5b89995575229d
DKrkc <92672947+DKrkc@users.noreply.github.com>
2021-11-10 21:52:01 +0300

    Update CheckLastName.java

--- Business/concretes/CheckLastName.java
+++ Business/concretes/CheckLastName.java
@@ -1,7 +1,7 @@
 package Business.concretes;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+//import java.util.regex.Matcher;
+//import java.util.regex.Pattern;
 
 import Business.abstracts.CheckFirstNameService;
 import Business.abstracts.CheckLastNameService;
@@ -12,7 +12,7 @@ public class CheckLastName implements CheckLastNameService {
 	@Override
 	public boolean checkLastName(Customer customer) {
 		
-		String regex ="[0-9a-zA-Z]{2,}";;
+		/* String regex ="[0-9a-zA-Z]{2,}";;
 		Pattern pattern = Pattern.compile(regex);
 		
 		Matcher matcher = pattern.matcher(customer.getLastName());
@@ -21,7 +21,13 @@ public class CheckLastName implements CheckLastNameService {
 				    	}else {
 				    		return false;
 	    		
-	    	}
+	    	}*/
+		
+		 if(customer.getLastName().length()>=2) {
+		 return true;
+	       }else {
+		 return false;
+	     }
 		
 	}
 
